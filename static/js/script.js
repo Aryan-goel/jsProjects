1a7c8b242cd0c8c11910eba713501fc9e33411e8
91639 <goel.aryan2406@gmail.com>
2022-04-05 16:48:26 +0530

    ageindays completed

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,11 @@
 //challenge 1
-function ageInDays () {
-var birthyear=prompt("your year of birth?")
+function ageInDays() {
+  var birthyear = prompt("your year of birth?");
+  var ageInDayss = (2022 - birthyear) * 365;
+  var h1 = document.createElement("h1");
+  var textAns = document.createTextNode(" you are " + ageInDayss + "days old");
+  h1.setAttribute("id", "ageInDays");
+  h1.appendChild(textAns);
+  document.getElementById('flex-box-result').appendChild(h1)
+  console.log(ageInDayss);
 }

14f78df13977233d6e286721fb1475c5acdf610a
91639 <goel.aryan2406@gmail.com>
2022-04-09 08:42:45 +0530

    challange 2:done

--- static/js/script.js
+++ static/js/script.js
@@ -4,12 +4,20 @@ function ageInDays() {
   var ageInDayss = (2022 - birthyear) * 365;
   var h1 = document.createElement("h1");
   var textAns = document.createTextNode(" you are " + ageInDayss + "days old");
-  h1.setAttribute("id", "ageInDays");
+  h1.setAttribute("idy", "ageInDays");
   h1.appendChild(textAns);
-  document.getElementById('flex-box-result').appendChild(h1)
+  document.getElementById("flex-box-result").appendChild(h1);
   console.log(ageInDayss);
 }
- 
+
 function reset() {
-    document.getElementById("ageInDays").remove();
+  document.getElementById("ageInDays").remove();
+}
+
+function generateCat() {
+  var image=document.createElement('img');
+  var div= document.getElementById('flex-cat-gen');
+  image.src="https://i.kym-cdn.com/photos/images/original/001/931/959/2e4.gif"
+  div.appendChild(image);
+
 }

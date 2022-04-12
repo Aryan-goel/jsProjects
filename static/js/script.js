a57863f45c2def01b8ededefc53fed800ede4642
91639 <goel.aryan2406@gmail.com>
2022-04-12 14:49:29 +0530

    refactoring

--- static/js/script.js
+++ static/js/script.js
@@ -25,12 +25,12 @@ function generateCat() {
 
 function rpsGame(yourChoice) {
   var humanChoice, botChoice;
-  humanChoice=yourChoice.id
-  botChoice =numberToChoice(randomToRpsInt())
-  console.log('computer choice: ',botChoice);
-  results=decideWinner(humanChoice,botChoice);
+  humanChoice = yourChoice.id;
+  botChoice = numberToChoice(randomToRpsInt());
+  console.log("computer choice: ", botChoice);
+  results = decideWinner(humanChoice, botChoice);
   console.log(results);
-  message=finalMessage(results)
+  message = finalMessage(results);
   console.log(message);
   rpsFrontEnd(yourChoice.id, botChoice, message);
 }
@@ -41,56 +41,58 @@ function randomToRpsInt() {
 function numberToChoice(number) {
   return ["rock", "paper", "scissors"][number];
 }
- 
-function decideWinner(yourChoice,computerChoice){
-  var rpsDatabase={
-    'rock':{'scissors':1,'rock':0.5,'paper':0},
-    'paper':{'rock':1,'paper':0.5,'scissors':0},
-    'scissors':{'paper':1,'scissors':0.5,'rock':0}
+
+function decideWinner(yourChoice, computerChoice) {
+  var rpsDatabase = {
+    rock: { scissors: 1, rock: 0.5, paper: 0 },
+    paper: { rock: 1, paper: 0.5, scissors: 0 },
+    scissors: { paper: 1, scissors: 0.5, rock: 0 },
+  };
+  var youScore = rpsDatabase[yourChoice][computerChoice];
+  var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+  return [youScore, computerScore];
+}
+function finalMessage([youScore, computerScore]) {
+  if (youScore === 0) {
+    return { message: "You lost!", color: "red" };
+  } else if (youScore === 0.5) {
+    return { message: "Tied!!", color: "yellow" };
+  } else {
+    return { message: "you won!", color: "green" };
   }
-  var youScore=rpsDatabase[yourChoice][computerChoice]
-  var computerScore=rpsDatabase[computerChoice][yourChoice]
-
-  return[youScore,computerScore];
-
 }
-function finalMessage([youScore,computerScore]){
-if(youScore===0){
-  return{'message':"You lost!",'color':'red'}
-   
-}
-else if(youScore===0.5){
-  return {'message':'Tied!!','color':"yellow"}
-}else{
-  return {'message':"you won!",'color':'green'}
-}
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
+  var imagesDatabase = {
+    rock: document.getElementById("rock").src,
+    paper: document.getElementById("paper").src,
+    scissors: document.getElementById("scissors").src,
+  };
+  //lets remove all the images
 
+  document.getElementById("rock").remove();
+  document.getElementById("paper").remove();
+  document.getElementById("scissors").remove();
 
-}
-function rpsFrontEnd(humanImageChoice,botImageChoice,finalMessage){
-var imagesDatabase={
-  'rock':document.getElementById("rock").src,
-  'paper':document.getElementById("paper").src,
-  'scissors':document.getElementById('scissors').src
+  var humanDiv = document.createElement("div");
+  var botDiv = document.createElement("div");
+  var messageDiv = document.createElement("div");
 
-
-}
-//lets remove all the images
-
-document.getElementById('rock').remove()
-document.getElementById('paper').remove()
-document.getElementById('scissors').remove()
-
-  var humanDiv=document.createElement('div');
-  var botDiv=document.createElement('div');
-  var messageDiv=document.createElement('div');
-
-  humanDiv.innerHTML="<img width src='" +imagesDatabase[humanImageChoice] +"' height=160 width=160 style='box-shadow:0px 10px 50px rgba(37,50,233,1);'>"
-  messageDiv.innerHTML="<h1 style='color:"+finalMessage['color']+"; font-size:60px padding:30px '>" + finalMessage['message'] +"</.h1>"
-  botDiv.innerHTML="<img width src='" +imagesDatabase[botImageChoice] +"' height=160 width=160 style='box-shadow:0px 10px 50px rgba(243,38,24,1);'>"
-  document.getElementById('flex-box-rps-div').append(humanDiv)
-  document.getElementById('flex-box-rps-div').append(messageDiv)
-  document.getElementById('flex-box-rps-div').append(botDiv)
-  
-
+  humanDiv.innerHTML =
+    "<img width src='" +
+    imagesDatabase[humanImageChoice] +
+    "' height=160 width=160 style='box-shadow:0px 10px 50px rgba(37,50,233,1);'>";
+  messageDiv.innerHTML =
+    "<h1 style='color:" +
+    finalMessage["color"] +
+    "; font-size:60px padding:30px '>" +
+    finalMessage["message"] +
+    "</.h1>";
+  botDiv.innerHTML =
+    "<img width src='" +
+    imagesDatabase[botImageChoice] +
+    "' height=160 width=160 style='box-shadow:0px 10px 50px rgba(243,38,24,1);'>";
+  document.getElementById("flex-box-rps-div").append(humanDiv);
+  document.getElementById("flex-box-rps-div").append(messageDiv);
+  document.getElementById("flex-box-rps-div").append(botDiv);
 }
